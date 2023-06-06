b13e3d6f64f5ea121c81cd0331c78e9664d5ddd5
Filipe <filipe@naveo.dev>
2023-06-06 16:28:41 +0200

    Created main packagewith command logic

---
+++ cmd/todo/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/FilipeFT/golang-todo"
+	"os"
+	"strings"
+)
+
+// Hardcoding the filename
+const todoFileName = ".todo.jason"
+
+func main() {
+
+	// Define an items list
+	l := &todo.List{}
+
+	// Use the Get method to read to do items from file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on the number of arguments provided
+	switch {
+
+	case len(os.Args) == 1:
+
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+
+	default:
+
+		item := strings.Join(os.Args[1:], " ")
+
+		l.Add(item)
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	}
+}
